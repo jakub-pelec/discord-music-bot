939d1e83c2436cefa3151292d3e03774624109a5
jakub-pelec <kuba1pelec@gmail.com>
2021-09-18 23:51:25 +0200

    removed doubled event listener

--- app.js
+++ app.js
@@ -20,27 +20,3 @@ client.player = player;
 register(client);
 
 client.login(token);
-
-client.on('interactionCreate', async(interaction) => {
-    const channel = interaction.member.voice.channel;
-    console.log(client.player.hasQueue())
-
-    if (channel) {
-        try {
-            await connectToChannel(channel);
-            const queue = client.player.createQueue(interaction.guild.id);
-            await queue.join(channel);
-            const song = await queue
-                .play(interaction.options.get("url").value)
-                .catch((_) => {
-                    if (!guildQueue) queue.stop();
-                });
-
-            interaction.reply("Playing new song!");
-        } catch (error) {
-            console.error(error);
-        }
-    } else {
-        interaction.reply("Join a voice channel then try again!");
-    }
-})
